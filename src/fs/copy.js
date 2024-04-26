d292d5b7bbab930bc1aa56418aab3c27ebbada26
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 14:40:14 +0300

    copy.js
    copy.js – реализовать функцию, которая копирует файлы из папки files
    со всем содержимым в папку files_copy на том же уровне (если папка
    files не существует или files_copy уже создана, должна быть выдана
    ошибка с сообщением FS operation failed)

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import { promises as fsPromises, constants } from 'fs';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const filesFolderPath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\fs\\files';
+    const filesCopyFolderPath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\fs\\files_copy';
+
+    try {       
+        await fsPromises.access(filesFolderPath, constants.F_OK);
+        await fsPromises.access(filesCopyFolderPath, constants.F_OK);
+        throw new Error('FS operation failed: Directory already exists');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            if (error.path === filesFolderPath || error.path === filesCopyFolderPath) {
+                await fsPromises.mkdir(filesCopyFolderPath);
+                const files = await fsPromises.readdir(filesFolderPath);
+                for (const file of files) {
+                    const sourceFilePath = path.join(filesFolderPath, file);
+                    const destFilePath = path.join(filesCopyFolderPath, file);
+                    await fsPromises.copyFile(sourceFilePath, destFilePath);
+                    console.log(`File ${file} copied successfully`);
+                }
+                console.log('All files copied successfully');
+            }
+        } else {
+            throw error;
+        }
+    } 
 };
 
 await copy();
