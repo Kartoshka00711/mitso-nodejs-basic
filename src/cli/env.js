12c8d6921a954f5ed2906751464bd9a8f271dd2e
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 15:02:47 +0300

    env.js
    поиск файлов с префиксом

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    console.log('MITSO environment variables:');
+    for (const key in process.env) {
+        if (key.startsWith('MITSO_')) {
+            console.log(`MITSO_${key}=${process.env[key]}`);
+        }
+    } 
 };
 
 parseEnv();
