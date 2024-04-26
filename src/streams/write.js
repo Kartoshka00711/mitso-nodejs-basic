ea001b839a050f03e0d203993e39a1339c46fb1e
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 16:39:49 +0300

    write.js
    хз, ничего не получилось

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\streams\\files\\fileToWrite.txt';
+
+    const writeStream = fs.createWriteStream(filePath, { encoding: 'utf-8' });
+
+    process.stdin.on('data', (chunk) => {
+        writeStream.write(chunk);
+    });
+
+    
+    writeStream.write('How die?\n');
+
+        process.stdin.on('end', () => {
+            writeStream.end();
+        console.log('Data writing completed.');
+    });
+
+        writeStream.on('error', (error) => {
+        console.error('Error writing data:', error);
+    }); 
 };
 
 await write();
