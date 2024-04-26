9738e2c9bc3386c51e1d8c6948bfdab7cdc51264
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 17:27:37 +0300

    Worker Threads (src/wt)

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { Worker, isMainThread } from 'worker_threads';
+import { cpus } from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    if (isMainThread) {
+        const cpuCount = cpus().length;
+        const results = [];
+
+        const workers = Array.from({ length: cpuCount }, (_, index) => {
+            const worker = new Worker('C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\wt\\worker.js', { workerData: index + 10 });
+
+            worker.on('message', (message) => {
+                results.push(message);
+                if (results.length === cpuCount) {
+                    console.log(results);
+                }
+            });
+
+            worker.on('error', (error) => {
+                results.push({ status: 'error', data: null });
+                if (results.length === cpuCount) {
+                    console.log(results);
+                }
+            });
+
+            return worker;
+        });
+
+        await Promise.all(workers.map(worker => new Promise(resolve => worker.on('exit', resolve))));
+    }
 };
 
 await performCalculations();
