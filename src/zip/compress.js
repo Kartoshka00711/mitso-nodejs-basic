2353d84072b60663cbbd300be3504b12805fa6d5
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 16:57:13 +0300

    compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const inputFilePath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\zip\\files\\fileToCompress.txt';
+    const outputFilePath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\zip\\files\\archive.gz';
+
+    const readStream = fs.createReadStream(inputFilePath);
+
+    const writeStream = fs.createWriteStream(outputFilePath);
+
+    const gzipStream = zlib.createGzip();
+
+    readStream.pipe(gzipStream).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('Compression completed.');
+    });
+
+    readStream.on('error', (error) => {
+        console.error('Error reading file:', error);
+    });
+
+    writeStream.on('error', (error) => {
+        console.error('Error writing file:', error);
+    });
 };
 
 await compress();
