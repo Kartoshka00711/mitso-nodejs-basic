4f8c3ca01442c25aee168a78d47a0eb5357ffa4d
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 14:52:31 +0300

    list.js
    list.js – реализовать функцию, которая выводит весь массив имен
    файлов из папки files в консоль (если папка files не существует, должна
    выдаваться ошибка с сообщением FS operation failed)

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import { promises as fsPromises, constants } from 'fs';
+
 const list = async () => {
-    // Write your code here 
+    const filesFolderPath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\fs\\files';
+
+    try {
+        await fsPromises.access(filesFolderPath, constants.F_OK);
+
+        const files = await fsPromises.readdir(filesFolderPath);
+
+        console.log('Files in directory:');
+        files.forEach(file => {
+            console.log(file);
+        });
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed: Directory does not exist');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await list();
