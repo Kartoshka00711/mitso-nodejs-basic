1b3b2b0e86dcc95e2c49355abe0255ce4146c825
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 14:55:10 +0300

    read.js
    read.js – реализовать функцию, которая выводит содержимое файла
    fileToRead.txt в консоль (если файла fileToRead.txt нет, должна
    выдаваться ошибка с сообщением FS operation failed)

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import { promises as fsPromises, constants } from 'fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const filesFolderPath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\fs\\files';
+    const fileToReadName = 'fileToRead.txt';
+    const filePath = path.join(filesFolderPath, fileToReadName);
+
+    try {
+        await fsPromises.access(filePath, constants.F_OK);
+
+        const fileContents = await fsPromises.readFile(filePath, 'utf-8');
+
+
+        console.log('File contents:');
+        console.log(fileContents);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed: File does not exist');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await read();
