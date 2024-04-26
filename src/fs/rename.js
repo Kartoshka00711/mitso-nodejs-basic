b3f65326aa3522ecdbc558c1920139706fdf2843
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 14:48:19 +0300

    rename.js
    rename.js – реализовать функцию, которая переименовывает файл
    wrongFilename.txt в properFilename с расширением .md (если файл
    wrongFilename.txt или properFilename.md уже существует, должна
    быть выдана ошибка с сообщением FS operation failed)

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import { promises as fsPromises, constants } from 'fs';
+import path from 'path';
+
 const rename = async () => {
-    // Write your code here 
+    const filesFolderPath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\fs\\files';
+    const wrongFileName = 'wrongFilename.txt';
+    const properFileName = 'properFilename.md';
+    const wrongFilePath = path.join(filesFolderPath, wrongFileName);
+    const properFilePath = path.join(filesFolderPath, properFileName);
+
+    try {
+        await fsPromises.access(wrongFilePath, constants.F_OK);
+        await fsPromises.access(properFilePath, constants.F_OK);
+        throw new Error('FS operation failed: File already exists');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            if (error.path === wrongFilePath || error.path === properFilePath) {
+                await fsPromises.rename(wrongFilePath, properFilePath);
+                console.log('File renamed successfully');
+            }
+        } else {
+            throw error;
+        }
+    } 
 };
 
 await rename();
