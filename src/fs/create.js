e7657d42ba0a95a487f593377dfcb08322289a85
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 14:30:47 +0300

    create.js
    create.js – реализовать функцию, которая создает новый файл
    fresh.txt с содержимым I am fresh and young внутри папки files
    (если файл уже существует, необходимо выдать Error с сообщением FS
    operation failed)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import { promises as fsPromises, constants } from 'fs';
+
 const create = async () => {
-    // Write your code here 
+    const filesFolderPath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\fs\\files';
+    const filePath = path.join(filesFolderPath, 'fresh.txt');
+
+    try {
+        await fsPromises.access(filePath, constants.F_OK);
+        throw new Error('FS operation failed: File already exists');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await fsPromises.writeFile(filePath, 'I am fresh and young');
+            console.log('File created successfully');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
