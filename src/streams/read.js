95821b2a3b2c46fc936cfd12efee8672e955da2d
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 16:29:45 +0300

    read.js
    реализовать функцию, которая читает содержимое файла
    fileToRead.txt с помощью Readable Stream и печатает его
    содержимое в process.stdout

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\streams\\files\\fileToRead.txt';
+
+    const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('end', () => {
+        console.log('\nFile reading completed.');
+    });
+
+    readStream.on('error', (error) => {
+        console.error('Error reading file:', error);
+    });
 };
 
 await read();
