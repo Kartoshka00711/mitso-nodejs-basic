565a4a92a3e741bcbbc4aaa3aa2357cb950dee4c
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 16:59:58 +0300

    decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const inputFilePath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\zip\\files\\archive.gz';
+    const outputFilePath = 'C:\\Users\\karto\\Documents\\GitHub\\mitso-nodejs-basic\\src\\zip\\files\\fileToDeCompress.txt';
+
+    const readStream = fs.createReadStream(inputFilePath);
+
+    const writeStream = fs.createWriteStream(outputFilePath);
+
+    const gunzipStream = zlib.createGunzip();
+
+    readStream.pipe(gunzipStream).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('Decompression completed.');
+    });
+
+    readStream.on('error', (error) => {
+        console.error('Error reading file:', error);
+    });
+
+    writeStream.on('error', (error) => {
+        console.error('Error writing file:', error);
+    });
 };
 
 await decompress();
