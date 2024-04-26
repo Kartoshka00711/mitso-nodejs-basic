bbda8730795a47a7a52e53d8f4bad05d9153d857
Kartoshka00711 <kartoshka00711@gmail.com>
2024-04-26 17:41:05 +0300

    cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    return new Promise((resolve, reject) => {
+        const childProcess = spawn('node', ['src\\cp\\files\\script.js', ...args], { stdio: ['pipe', 'pipe', 'inherit'] });
+
+        process.stdin.pipe(childProcess.stdin);
+        childProcess.stdout.pipe(process.stdout);
+
+        childProcess.on('exit', (code) => {
+            if (code === 0) {
+                resolve();
+            } else {
+                reject(new Error(`Child process exited with code ${code}`));
+            }
+        });
+
+        childProcess.on('error', (error) => {
+            reject(error);
+        });
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2' , 'someArgument1','someArgument1','someArgument1',]);
